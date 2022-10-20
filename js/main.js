746d1da405bedd30d12c971b702b25068f3df8ee
muac404 <muac404@gmail.com>
2022-10-20 22:00:40 +0200

    Aggiunge risultato html

--- js/main.js
+++ js/main.js
@@ -9,13 +9,16 @@ const fullPrice = km * 0.21
 console.log("prezzo di listino",fullPrice);
 //CONDIZIONI DI SCONTO
 
+
 if (age < 18) {
     console.log("requisito sconto minorenne") 
     let finalPrice = fullPrice - (fullPrice * 0.2)
     let roundedPrice = finalPrice.toFixed(2)
-    console.log("prezzo con sconto giovane",roundedPrice)
+    console.log("prezzo con sconto young",roundedPrice)
+    document.getElementById("prezzo-biglietto").innerHTML=roundedPrice + " €";
 } else {
     let roundedPrice = fullPrice.toFixed(2)
+    document.getElementById("prezzo-biglietto").innerHTML=roundedPrice + " €";
     
 } 
 
@@ -23,9 +26,11 @@ if (age >= 65) {
     console.log("requisito sconto over65")  
     let finalPrice = fullPrice - (fullPrice * 0.4)
     let roundedPrice = finalPrice.toFixed(2)
-    console.log("prezzo con sconto over",roundedPrice)
+    console.log("prezzo con sconto over65",roundedPrice)
+    document.getElementById("prezzo-biglietto").innerHTML=roundedPrice + " €";
 } else {
     let roundedPrice = fullPrice.toFixed(2)
+    document.getElementById("prezzo-biglietto").innerHTML=roundedPrice + " €";
     
 }
 
