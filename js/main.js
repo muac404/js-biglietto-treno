9ed9039acbea2ca37c6e4b7bd29c6b6ff9141c7a
muac404 <muac404@gmail.com>
2022-10-20 20:49:37 +0200

    Aggiunge condizioni sconto

--- js/main.js
+++ js/main.js
@@ -4,23 +4,28 @@ const km = Number(prompt("Inserisci i km che vuoi percorrere"));
 const age = Number(prompt("Inserisci la tua età"));
 //CALCOLO IL PREZZO DEL BIGLIETTO INTERO (0,21 EURO AL KM)
 const fullPrice = km * 0.21
-console.log(fullPrice);
+console.log("prezzo di listino",fullPrice);
 //SE L'ETA' E MINORE DI 18 ANNI APPLICO SCONTO 20%
+
 if (age < 18) {
      
-    const discountedPrice = fullPrice - (fullPrice * 0.2)
-    console.log(discountedPrice)
+    let finalPrice = fullPrice - (fullPrice * 0.2)
+    console.log("prezzo con sconto giovane",finalPrice)
 } else {
-    const finalPrice = Fullprice
-}
+    let finalPrice = fullPrice
+    
+} 
 
 if (age >= 65) {
      
-    const discountedPrice = fullPrice - (fullPrice * 0.4)
-    console.log(discountedPrice)
+    let finalPrice = fullPrice - (fullPrice * 0.4)
+    console.log("prezzo con sconto over",finalPrice)
 } else {
-    const finalPrice = Fullprice
+    let finalPrice = fullPrice
+    
 }
+
+
 // ALTRIMENTI PREZZO INTERO
 //SE L'ETA' E' MAGGIORE O UGUALE A 65 ANNI APPLICO SCONTO DEL 40%
 // ALTRIMENTI PREZZO INTERO
