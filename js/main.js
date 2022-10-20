798912dd68fe678f5d21a994e7a717af71c4baae
muac404 <muac404@gmail.com>
2022-10-20 21:42:13 +0200

    Corregge errore di digitazione

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
+
+
 //CHIEDO ALL'UTENTE I KM CHE VUOLE PERCORRERE
 const km = Number(prompt("Inserisci i km che vuoi percorrere"));
 //CHIEDO ALL'UTENTE LA SUA ETA'
 const age = Number(prompt("Inserisci la tua età"));
-//CALCOLO IL PREZZO DEL BIGLIETTO INTERO (0,21 EURO AL KM)
+//CALCOLO IL PREZZO DEL BIGLIETTO INTERO (0.21 EURO AL KM)
 const fullPrice = km * 0.21
 console.log("prezzo di listino",fullPrice);
 //CONDIZIONI DI SCONTO
 
 if (age < 18) {
-     
+    console.log("requisito sconto minorenne") 
     let finalPrice = fullPrice - (fullPrice * 0.2)
     let roundedPrice = finalPrice.toFixed(2)
     console.log("prezzo con sconto giovane",roundedPrice)
@@ -18,7 +20,7 @@ if (age < 18) {
 } 
 
 if (age >= 65) {
-     
+    console.log("requisito sconto over65")  
     let finalPrice = fullPrice - (fullPrice * 0.4)
     let roundedPrice = finalPrice.toFixed(2)
     console.log("prezzo con sconto over",roundedPrice)
@@ -26,6 +28,9 @@ if (age >= 65) {
     let roundedPrice = fullPrice.toFixed(2)
     
 }
+
+
+
 
 
 
